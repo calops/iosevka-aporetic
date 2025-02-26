e27135bf4b9c282c83367a69a10fd75c63900145
Rémi Labeyrie <calops@tocards.net>
2025-02-26 11:45:21 +0100

    add hash for mono

--- hashes.nix
+++ hashes.nix
@@ -1,4 +1,4 @@
 {
   aporetic-sans = "sha256-gaNbLmuO0sIs54bwnI6Jp9BeV5/S/IJvSAJCm2ruKIY=";
-  aporetic-sans-mono = "";
+  aporetic-sans-mono = "sha256-DYKWdnGgAhEP+f1vK55bsCdRtffJsWB8uHTBFmH/NF8=";
 }
